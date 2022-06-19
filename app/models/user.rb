d6bc0eff7f6d5b2c68c0f6e555261b17851aefa7
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-06-19 19:05:23 +0500

    Create user model

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :password_digest, presence: true
+end
